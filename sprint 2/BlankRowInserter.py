04dca7e967cd2aa7d8827aa1091d5ec8a79b6d29
marceloehbemlegal <krawby@hotmail.com>
2017-05-04 07:59:48 -0300

    Update BlankRowInserter.py

--- sprint 2/BlankRowInserter.py
+++ sprint 2/BlankRowInserter.py
@@ -1,3 +1,7 @@
+#! python3
+# BlankRowInserter.py - Inserts a given amount of rows from the given starting row in a sheet
+
+
 import openpyxl
 
 
@@ -11,10 +15,10 @@ M = int(input())
 
 rowBlank = 1
 
-for row in range(1, 200+M):
-    if row == N:
+for row in range(1, wb.max_row+1+M):  # from row 1 to the max row of the original sheet plus the amount to be inserted
+    if row == N: # when the current row is the given starting point, skips the given amount of rows
         rowBlank += M
-    for column in range(1, 200):
+    for column in range(1, wb.max_column+1): # copies the content from the original sheet to the new one
         sheetBlankRow.cell(row=rowBlank, column=column).value = sheet.cell(row=row, column=column).value
     rowBlank += 1
 
