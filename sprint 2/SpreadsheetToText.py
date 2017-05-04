be54c98c9d6cda007fb73789fe74b64ddb4f788b
marceloehbemlegal <krawby@hotmail.com>
2017-05-04 08:23:46 -0300

    Update SpreadsheetToText.py

--- sprint 2/SpreadsheetToText.py
+++ sprint 2/SpreadsheetToText.py
@@ -11,7 +11,7 @@ sheet = wb.get_active_sheet()
 for column in range(1, sheet.max_column+1):
     file = open('spreadsheetToText_%03d.txt' % column, 'w')  # new file for each new column
     for row in range(1, sheet.max_row+1):
-        if sheet.cell(row=row, column=column).value == None: # if sheet is blank, prints end of line
+        if sheet.cell(row=row, column=column).value == None: # if cell is blank, prints end of line
             file.write('\n')
         else:
             file.write(str(sheet.cell(row=row, column=column).value)+'\n')
