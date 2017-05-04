c909d5b7cef60d192ba924e14ed24be008059f0d
marceloehbemlegal <krawby@hotmail.com>
2017-05-04 08:03:37 -0300

    Update CellInverter.py

--- sprint 2/CellInverter.py
+++ sprint 2/CellInverter.py
@@ -1,3 +1,7 @@
+#! python3
+# CellInverter.py - Takes a sheet and shifts the rows into columns and vice versa
+
+
 import openpyxl
 
 
@@ -10,12 +14,12 @@ sheetInverted = wbInverted.get_active_sheet()
 sheetList = []
 
 for row in range(1, sheet.max_row+1):
-    rowList = []
-    for column in range(1, sheet.max_column+1):
+    rowList = [] 
+    for column in range(1, sheet.max_column+1): # saves all values from the current row into 'rowList'
         rowList.append(sheet.cell(row=row, column=column).value)
-    sheetList.append(rowList)
+    sheetList.append(rowList) # each element in 'sheetList' will be a list that contains a row from the sheet
 
-for row in range(1, sheet.max_column+1):
+for row in range(1, sheet.max_column+1): # copies the original row and prints it as a column in the new sheet
     for column in range(1, sheet.max_row+1):
         sheetInverted.cell(row=row, column=column).value = sheetList[column-1][row-1]
 
