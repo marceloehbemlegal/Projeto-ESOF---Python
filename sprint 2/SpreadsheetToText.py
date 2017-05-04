78fda7d91e84aad4e4794837fcaecf0e9c98b79c
marceloehbemlegal <krawby@hotmail.com>
2017-05-04 08:23:31 -0300

    Update SpreadsheetToText.py

--- sprint 2/SpreadsheetToText.py
+++ sprint 2/SpreadsheetToText.py
@@ -1,3 +1,7 @@
+#! python3
+# SpreadsheetToText.py - Loads a sheet and copies each column into a separate text file
+
+
 import openpyxl
 
 
@@ -5,9 +9,9 @@ wb = openpyxl.load_workbook('multiplication_table.xlsx')
 sheet = wb.get_active_sheet()
 
 for column in range(1, sheet.max_column+1):
-    file = open('spreadsheetToText_%03d.txt' % column, 'w')
+    file = open('spreadsheetToText_%03d.txt' % column, 'w')  # new file for each new column
     for row in range(1, sheet.max_row+1):
-        if sheet.cell(row=row, column=column).value == None:
+        if sheet.cell(row=row, column=column).value == None: # if sheet is blank, prints end of line
             file.write('\n')
         else:
             file.write(str(sheet.cell(row=row, column=column).value)+'\n')
