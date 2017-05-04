eb2324118659214729f2d1bb31f7b83218e653aa
marceloehbemlegal <krawby@hotmail.com>
2017-05-04 08:26:38 -0300

    Update TextFileToSpreadsheet.py

--- sprint 2/TextFileToSpreadsheet.py
+++ sprint 2/TextFileToSpreadsheet.py
@@ -1,3 +1,7 @@
+#! python3
+# TextFileToSpreadsheet.py - Reads several files and prints the content of the lines into the rows of a column for each one
+
+
 import openpyxl
 
 
@@ -6,8 +10,8 @@ sheet = wb.get_active_sheet()
 
 for i in range(11):
     file = open('spreadsheetToText_%03d.txt' % (i+1), 'r')
-    lines = file.readlines()
+    lines = file.readlines() # 'lines' -> list in which each elements is a line from the file read
     for j in range(len(lines)):
-        sheet.cell(row=j+1, column=i+1).value = lines[j]
+        sheet.cell(row=j+1, column=i+1).value = lines[j] # prints each line into each row
 
 wb.save('textToSpreadsheet.xlsx')
